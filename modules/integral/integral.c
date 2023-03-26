a1098cd3097124459ac7545f28e4b55f598feec1
Vitor Hideyoshi Nakazone Batista <vitor.h.n.batista@gmail.com>
2023-03-26 14:55:24 -0300

    FInal Double Simpson Implementation

--- modules/integral/integral.c
+++ modules/integral/integral.c
@@ -1,10 +1,8 @@
 #include "integral.h"
 
 
-double _partition(double a, double b, int n, int i) {
-    float delta = (b-a)/n;
+double _partition(double a, double delta, int i) {
     float x = a + i*delta;
-
     return x;
 }
 
@@ -22,20 +20,17 @@ double riemman_simple(double (*f)(double), double a, double b, int n) {
 double simpson_simple(double (*f)(double), double a, double b, int n) {
     double delta = (b-a)/n;
 
-
     double eta = 0;
+    for (int psi = 1; psi <= (n/2); psi++) {
+        eta = eta + f(_partition(a, delta, 2*psi - 1));
+    }
+
     double theta = 0;
-    double psi = 1;
-    double kappa = 1;
-
-    for (int psi = 0; psi <= (n/2); psi++) {
-        eta = eta + f(_partition(a, b, n, 2*psi - 1));
-    }
-    for (int kappa = 0; kappa <= ((n/2)-1); kappa++) {
-        theta = theta + f(_partition(a, b, n, 2*kappa));
+    for (int kappa = 1; kappa <= ((n/2)-1); kappa++) {
+        theta = theta + f(_partition(a, delta, 2*kappa));
     }
 
-    return (delta/3)*( f(_partition(a, b, n, 0)) + f(_partition(a, b, n, n)) + 4*eta + 2*theta);
+    return (delta/3)*( f(_partition(a, delta, 0)) + f(_partition(a, delta, n)) + 4*eta + 2*theta);
 }
 
 double riemann_double(double (*f)(double, double), double a, double b, double c, double d, int n) {
@@ -52,7 +47,7 @@ double riemann_double(double (*f)(double, double), double a, double b, double c,
     return theta*(dx)*(dy);
 }
 
-double g(double (*f)(double, double), double a, double b, double c, double d, int n, int i) {
+double _g(double (*f)(double, double), double a, double b, double c, double d, int n, int i) {
     double dx = (b-a)/n;
     double dy = (d-c)/n;
 
@@ -61,15 +56,15 @@ double g(double (*f)(double, double), double a, double b, double c, double d, in
 
     double sigma = 0;
     for(int zeta = 1; zeta <= (n/2); zeta++) {
-        sigma += f(_partition(a, b, n, i),_partition(c, d, n, 2*zeta - 1));
+        sigma += f(_partition(a, dx, i),_partition(c, dy, 2*zeta - 1));
     }
 
     double upsilon  = 0;
     for (int csi = 1; csi <= ((n/2)-1); csi++) {
-        upsilon  += f(_partition(a, b, n, i),_partition(c, d, n, 2*csi));
+        upsilon  += f(_partition(a, dx, i),_partition(c, dy, 2*csi));
     }
 
-    return (dy/3)*( f(_partition(a, b, n, i),_partition(c, d, n, 0)) + f(_partition(a, b, n, i),_partition(c, d, n, n)) + 4*sigma + 2*upsilon );
+    return (dy/3)*( f(_partition(a, dx, i),_partition(c, dy, 0)) + f(_partition(a, dx, i),_partition(c, dy, n)) + 4*sigma + 2*upsilon );
 }
 
 double simpson_double(double (*f)(double, double), double a, double b, double c, double d, int n) {
@@ -79,13 +74,13 @@ double simpson_double(double (*f)(double, double), double a, double b, double c,
 
     double eta = 0;
     for (int psi = 1; psi < (n/2); psi++) {
-        eta += g(f, a, b, c, d, n, 2*psi - 1);
+        eta += _g(f, a, b, c, d, n, 2*psi - 1);
     }
 
     double theta = 0;
     for (int kappa = 1; kappa < ((n/2)-1); kappa++) {
-        theta += g(f, a, b, c, d, n, 2*kappa);
+        theta += _g(f, a, b, c, d, n, 2*kappa);
     }
 
-    return (dx/3)*( g(f, a, b, c, d, n, 0) + g(f, a, b, c, d, n, n) + 4*eta + 2*theta);
+    return (dx/3)*( _g(f, a, b, c, d, n, 0) + _g(f, a, b, c, d, n, n) + 4*eta + 2*theta);
 }
